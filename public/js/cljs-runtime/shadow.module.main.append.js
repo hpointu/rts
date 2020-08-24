9780aeebeddd7cd1187e6a956de1b164d9c464df
freddy <tedefump@gmail.com>
2020-08-24 09:50:25 +0100

    Push compiled version

--- public/js/cljs-runtime/shadow.module.main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('main');
-
-try { hpointu.rts.app.init(); } catch (e) { console.error("An error occurred when calling (hpointu.rts.app/init)"); throw(e); }
